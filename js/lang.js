d8690c3520902805b5e58d7d011757200ee32c1c
Rodrigo <rodrigo@addmind.me>
2025-03-10 23:38:19 -0300

    Lang chang fix

--- js/lang.js
+++ js/lang.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     selectElement.addEventListener('change', (e) =>{
         if(selectElement.options[selectElement.selectedIndex].value == 'US'){
-            window.location.href = '/en.html' 
+            window.location.href = 'en.html' 
         } else{
             window.location.href = '/index.html';
         }
